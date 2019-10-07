3a51367cafff2fabc581e969d9d22593fa5be2e1
mathiaslyngbye <malyn16@student.sdu.dk>
2019-10-07 23:20:40 +0200

    Add simple file import

--- main.cpp
+++ main.cpp
@@ -1,7 +1,56 @@
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main(void)
+bool import(std::vector<std::string> &map, std::string filename)
 {
-    printf("Hello!\n");
+    std::ifstream mapfile;
+    mapfile.open(filename);
+    
+    if(!mapfile.is_open())
+    {
+        return false;
+    }
+    
+    std::string mapline;
+    while(!mapfile.eof())
+    {   
+        getline(mapfile,mapline);
+        map.push_back(mapline);
+    }
+    
+    mapfile.close();
+
+    return true;
+}
+
+
+int main(int argc, char *argv[])
+{   
+    std::string filename;
+    std::vector<std::string> map;
+
+    if(argc != 2)
+    {
+        std::cout << "Invalid number of arguments" << std::endl;
+        return -1;
+    }
+    else
+    {
+        filename = argv[1];
+    }
+    
+    if(!import(map, filename))
+    {
+        std::cout << "Failed to import file " << filename << std::endl;
+        return -1;
+    }
+    
+    for(std::vector<std::string>::iterator i = map.begin(); i != map.end(); ++i)
+    {
+        std::cout << *i << std::endl;
+    }
+
     return 0;
 }
