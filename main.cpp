240cc5b88603cada21c8921731a5142d345b65bb
mathiaslyngbye <malyn16@student.sdu.dk>
2019-10-08 20:02:42 +0200

    Cleaned up code. Strongly consider moving to class

--- main.cpp
+++ main.cpp
@@ -17,14 +17,14 @@ typedef struct
     std::vector<std::vector<char>> wall;
 } board;
 
-/*  Import map file:
-    &map    String vector for storing each line.
-    mapfile String name (and location) of file to be imported */
-bool import(std::vector<std::string> &map, std::string filename)
+//  Import map file:
+//  &map    String vector for storing each line.
+//  mapfile String name (and location) of file to be imported 
+bool import(std::vector<std::string> &t_map, std::string t_filename)
 {
     // Attempt to open map file
     std::ifstream mapfile;
-    mapfile.open(filename);
+    mapfile.open(t_filename);
     
     // Check if file can be opened (exists)
     if(!mapfile.is_open())
@@ -37,7 +37,7 @@ bool import(std::vector<std::string> &map, std::string filename)
     while(!mapfile.eof())
     {   
         getline(mapfile,mapline);   // Save line to temp variable
-        map.push_back(mapline);     // Push variable to map vector
+        t_map.push_back(mapline);     // Push variable to map vector
     }
    
     // Close map file
@@ -46,89 +46,88 @@ bool import(std::vector<std::string> &map, std::string filename)
     return true;
 }
 
-/*  Print contents of board and player structures,
-    in same map */
-void print(board board, point player)
+// Print contents of board and player structures,in same map
+void print(board t_board, point t_player)
 {    
-    for(unsigned int i = 0; i < board.wall.size(); i++)
+    for(unsigned int i = 0; i < t_board.wall.size(); i++)
     {
-        for(unsigned int j = 0; j < board.wall[i].size(); j++)
+        for(unsigned int j = 0; j < t_board.wall[i].size(); j++)
         {
-            if(player.x == j && player.y == i)
+            if(t_player.x == j && t_player.y == i)
             {
                 std::cout << 'M';
             }
-            else if(board.box[i][j] != ' ')
+            else if(t_board.box[i][j] != ' ')
             {
-                std::cout << board.box[i][j];
+                std::cout << t_board.box[i][j];
             }
-            else if(board.goal[i][j] != ' ')
+            else if(t_board.goal[i][j] != ' ')
             {
-                std::cout << board.goal[i][j];
+                std::cout << t_board.goal[i][j];
             }
             else
             {
-                std::cout << board.wall[i][j];
+                std::cout << t_board.wall[i][j];
             }
         }
         std::cout << std::endl;
     }
 }
 
-/*  Map parser. Splits map up into various objects, 
-    and stores them in board and player structures */
-void parse(std::vector<std::string> map, board &board, point &player)
+// Map parser. Splits map up into various objects, 
+// and stores them in board and player structures 
+void parse(std::vector<std::string> t_map, board &t_board, point &t_player)
 {
-    /*  Character representation:
-        .: walkable positions; 
-        X: wall; 
-        M: start position of robot; 
-        J: start positions of cans; 
-        G: Goal positions of cans;
-    */
+    //  Character representation:
+    //  .: walkable positions; 
+    //  X: wall; 
+    //  M: start position of robot; 
+    //  J: start positions of cans; 
+    //  G: Goal positions of cans;
+
     std::vector<char> tmp_box;
     std::vector<char> tmp_goal;
     std::vector<char> tmp_wall;
-    for(unsigned int i = 0; i < map.size(); i++)
+    for(unsigned int i = 0; i < t_map.size(); i++)
     {
-        for(unsigned int j = 0; j < map[i].size(); j++)
+        for(unsigned int j = 0; j < t_map[i].size(); j++)
         {
-            if(map[i][j] == ' ')
+            if(t_map[i][j] == ' ')
             {
                 tmp_box.push_back(' ');
                 tmp_goal.push_back(' ');
                 tmp_wall.push_back(' ');
             }
-            else if(map[i][j] == 'X' || map[i][j] == '.')
+            else if(t_map[i][j] == 'X' || t_map[i][j] == '.')
             {
-                tmp_wall.push_back(map[i][j]);
+                tmp_wall.push_back(t_map[i][j]);
                 tmp_box.push_back(' ');
                 tmp_goal.push_back(' ');
             }
-            else if(map[i][j] == 'G')
+            else if(t_map[i][j] == 'G')
             {
                 tmp_wall.push_back('.');
                 tmp_box.push_back(' ');
-                tmp_goal.push_back(map[i][j]);
+                tmp_goal.push_back(t_map[i][j]);
             }
-            else if(map[i][j] == 'J')
+            else if(t_map[i][j] == 'J')
             {
                 tmp_wall.push_back('.');
-                tmp_box.push_back(map[i][j]);
+                tmp_box.push_back(t_map[i][j]);
                 tmp_goal.push_back(' ');
             }
-            else if(map[i][j] == 'M')
+            else if(t_map[i][j] == 'M')
             {
                 tmp_wall.push_back('.');
                 tmp_box.push_back(' ');
                 tmp_goal.push_back(' ');
-                player.x = j;
-                player.y = i;
+                t_player.x = j;
+                t_player.y = i;
             }
         }
-        board.wall.push_back(tmp_wall);
-        board.box.push_back(tmp_box);
-        board.goal.push_back(tmp_goal);
+        t_board.wall.push_back(tmp_wall);
+        t_board.box.push_back(tmp_box);
+        t_board.goal.push_back(tmp_goal);
         tmp_wall.clear();
         tmp_box.clear();
         tmp_goal.clear();
@@ -141,10 +140,10 @@ int main(int argc, char *argv[])
     std::string filename;
     std::vector<std::string> map;
 
-/*  Check function argument count
-    Note: We want 2 arguments:
-        1) Function call
-        2) Location of map file */
+    // Check function argument count
+    // Note: We want 2 arguments:
+    // 1) Function call
+    // 2) Location of map file 
     if(argc != 2)
     {
         std::cout << "Invalid number of arguments" << std::endl;
@@ -163,12 +162,12 @@ int main(int argc, char *argv[])
     }
     
     // Create objects for storing parser results
-    board Board;
-    point Player;
+    board testBoard;
+    point testPlayer;
 
     // parse and print map for demonstration
-    parse(map, Board, Player);
-    print(Board, Player);
+    parse(map, testBoard, testPlayer);
+    print(testBoard, testPlayer);
 
     return 0;
 }
