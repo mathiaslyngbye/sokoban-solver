2c68d8fba4972b315f347adc6804fdc125e1fcc6
mathiaslyngbye <malyn16@student.sdu.dk>
2019-10-10 13:04:34 +0200

    Reduce move function complexity drastically

--- sokoban.cpp
+++ sokoban.cpp
@@ -25,55 +25,58 @@ void Sokoban::move(int t_dx, int t_dy)
     int agent_m1 = m_agent + (t_dx + t_dy*m_cols);
     int agent_m2 = m_agent + (2*t_dx + 2*t_dy*m_cols);
     
-    if (isWall(m_board[agent_m1]) ||
-       (isBox(m_board[agent_m1]) && isWall(m_board[agent_m2])) ||
-       (isBox(m_board[agent_m1]) && isBox(m_board[agent_m2])))
+    
+    if(isBox(m_board[agent_m1]) && isFree(m_board[agent_m2]))
+        moveCell(m_board[agent_m1], m_board[agent_m2]);
+    
+    if(isFree(m_board[agent_m1]))
     {
-        ;
-    }
-    else
-    {
-        // This is disgusting
-        if(isBox(m_board[agent_m1]))
-        {
-            if(isGoal(m_board[agent_m1]))
-                m_board[agent_m1] = 'G';
-            else
-                m_board[agent_m1] = '.';
-
-            if(isGoal(m_board[agent_m2]))
-                m_board[agent_m2] = 'j';
-            else
-                m_board[agent_m2] = 'J';
-        }
-        
-        if(isGoal(m_board[m_agent]))
-            m_board[m_agent] = 'G';
-        else
-            m_board[m_agent] = '.';
-
-        if(isGoal(m_board[agent_m1]))
-            m_board[agent_m1] = 'm';
-        else
-            m_board[agent_m1] = 'M';
-        
+        moveCell(m_board[m_agent], m_board[agent_m1]);
         m_agent = agent_m1;
     }
 }
 
-bool Sokoban::isGoal(char t_tile)
+void Sokoban::moveCell(char &t_src, char &t_dst)
 {
-    return (t_tile == 'm' || 
-            t_tile == 'j' || 
-            t_tile == 'G');
+    if(isGoal(t_dst))
+    {   
+        if(isBox(t_src))
+            t_dst = 'j';
+        else
+            t_dst = 'm';
+    }
+    else
+    {
+        if(isBox(t_src))
+            t_dst = 'J';
+        else
+            t_dst = 'M';
+    }
+
+    if(isGoal(t_src))
+        t_src = 'G';
+    else
+        t_src = '.';
 }
 
-bool Sokoban::isBox(char t_tile)
+bool Sokoban::isGoal(char t_cell)
 {
-    return (t_tile == 'J' || t_tile == 'j');
+    return (t_cell == 'm' || 
+            t_cell == 'j' || 
+            t_cell == 'G');
 }
 
-bool Sokoban::isWall(char t_tile)
+bool Sokoban::isBox(char t_cell)
 {
-    return (t_tile == '#');
+    return (t_cell == 'J' || t_cell == 'j');
+}
+
+bool Sokoban::isWall(char t_cell)
+{
+    return (t_cell == '#');
+}
+
+bool Sokoban::isFree(char t_cell)
+{
+    return (t_cell == '.' || t_cell == 'G');
 }
--- sokoban.hpp
+++ sokoban.hpp
@@ -11,9 +11,11 @@ public:
     void print();
     void move(int t_dx, int t_dy);
 private:
-    bool isBox(char t_tile);
-    bool isGoal(char t_tile);
-    bool isWall(char t_tile);
+    void moveCell(char &t_src, char &t_dst);
+    bool isBox(char t_cell);
+    bool isGoal(char t_cell);
+    bool isWall(char t_cell);
+    bool isFree(char t_cell);
     std::string m_board;
     int m_rows;
     int m_cols;
